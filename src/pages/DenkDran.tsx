d80439e51ce9e61a90ca5650142608e4fd332276
lordgreyt <falko.sontopski@googlemail.com>
2026-05-20 09:19:24 +0200

    korrekte zuweisung der tage "Denk dran"

--- src/pages/DenkDran.tsx
+++ src/pages/DenkDran.tsx
@@ -21,7 +21,10 @@ function getMonday(d: Date): Date {
 }
 
 function formatDate(d: Date): string {
-  return d.toISOString().split('T')[0];
+  const y = d.getFullYear();
+  const m = String(d.getMonth() + 1).padStart(2, '0');
+  const day = String(d.getDate()).padStart(2, '0');
+  return `${y}-${m}-${day}`;
 }
 
 function sameDay(a: Date, b: Date): boolean {
